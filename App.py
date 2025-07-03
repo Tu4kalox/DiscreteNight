78b53bcd03bebb6ecd89f05bca1e2da5054fd79d
Tu4kalox <teacloud999@gmail.com>
2025-07-03 11:52:00 +0700

    Собрал полурабочий билд

--- App.py
+++ App.py
@@ -1,38 +1,134 @@
+from turtle import update
+from kivy.config import Config
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '600')
+from os import close
+from tokenize import String
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.uix.button import Button
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, StringProperty
+from pyproj import Transformer
+from Classes.Transmitter import Transmitter
+
 
 
 Builder.load_file('UI.kv')
 
-AntsForOpt = []
-Ants = open('antennas.txt')
-for ant in Ants:
+class MenuScreen(Screen):
+    def __init__(self, **kwargs):
+       super().__init__(**kwargs)
+       self.load_data()
 
-
-class MenuScreen(Screen): pass
-class CalcScreen(Screen):
-    def calculate(self):
+    def load_data(self):
         try:
-            ant = list(map(float, self.ids.ant_input.text.split(';')))
-            sat = list(map(float, self.ids.sat_input.text.split(';')))
-            result = f"Расстояние между антенной и спутником рассчитать тяжело {ant[1]}"
-            self.ids.result_label.text = result
+            with open('antennas.txt', 'r', encoding='utf-8') as f:
+                lines = f.read().splitlines()
+            App.get_running_app().antennas_list = lines
+
+        except FileNotFoundError:
+            self.ids.debug.text += '\nТы зачем удалил тхтшник c антеннами?'
+
+        try:
+            with open('satellites.txt', 'r', encoding='utf-8') as f:
+                lines = f.read().splitlines()
+            App.get_running_app().satellites_list = lines
+        except FileNotFoundError:
+            self.ids.debug.text +='\nА тхт со спутниками чоте сделал?'
+
+    def save_and_exit(self):
+        with open('antennas.txt', 'w', encoding='utf-8') as f:
+            f.writelines(line + '\n' for line in App.get_running_app().antennas_list)
+        with open('satellites.txt', 'w', encoding='utf-8') as f:
+            f.writelines(line + '\n' for line in App.get_running_app().satellites_list)
+        App.get_running_app().stop()
+
+
+class CalcScreen(Screen):
+    def calculate(self, direction='from_antenna'):
+        try:
+            selected_antenna_name = self.ids.antenna_spinner.text
+            selected_satellite_name = self.ids.satellite_spinner.text
+
+            with open('antennas.txt', 'r', encoding='utf-8') as f:
+                lines = f.read().splitlines()
+                for line in lines:
+                    params = line.split(';')
+                    if params[0] == selected_antenna_name:
+                        cords = self.ids.ant_cords_input.text.split(';')
+                        antenna = Transmitter(*params[1:7], *cords, True)
+
+            with open('satellites.txt', 'r', encoding='utf-8') as f:
+                lines = f.read().splitlines()
+                for line in lines:
+                    params = line.split(';')
+                    if params[0] == selected_satellite_name:
+                        cords = self.ids.sat_cords_input.text.split(';')
+                        satellite = Transmitter(*params[1:7], *cords, False)
+
+            other_params = self.ids.other_params_input.text.split(';')
+
+            if direction == 'from_antenna':
+                result = antenna.Calculate_signal_noise_ratio(satellite, *other_params)
+            else:
+                result = satellite.Calculate_signal_noise_ratio(antenna, *other_params)
+
         except:
-            self.ids.result_label.text = "Ошибка: некорректный ввод"
+            result = 'Ошибка: некорректный ввод'
+
+        self.ids.result_label.text = str(result)
 
 class OptimizeScreen(Screen): 
     def optimize(self):
+        pass
+
+
+class BaseConstructorScreen(Screen):
+    file_name = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class AntennaConstructorScreen(BaseConstructorScreen):
+    title_text = "Конструктор антенны"
+    choose_text = "Выберете антенну"
+    file_name = "antennas.txt"
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class SatelliteConstructorScreen(BaseConstructorScreen):
+    title_text = "Конструктор спутника"
+    choose_text = "Выберете спутник"
+    file_name = "satellites.txt"
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
 class CalcApp(App):
+    antennas_list = ListProperty()
+    satellites_list = ListProperty()
+    antennas_names = ListProperty()
+    satellites_names = ListProperty()
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(antennas_list = self.update_antennas_names)
+        self.bind(satellites_list = self.update_satellites_names)
+
+    def update_antennas_names(self, instance, value):
+        self.antennas_names = [line.split(';')[0] for line in value]
+
+    def update_satellites_names(self, instance, value):
+        self.satellites_names = [line.split(';')[0] for line in value]
+
     def build(self):
         sm = ScreenManager()
         
         with sm.canvas.before:
-            from kivy.graphics import Color, Rectangle2
+            from kivy.graphics import Color, Rectangle
             Color(0.07, 0.07, 0.07, 1) 
             self.bg_rect = Rectangle(size=sm.size, pos=sm.pos)
 
@@ -41,6 +137,8 @@ class CalcApp(App):
         sm.add_widget(MenuScreen(name="menu"))
         sm.add_widget(CalcScreen(name="calc"))
         sm.add_widget(OptimizeScreen(name="optimize"))
+        sm.add_widget(AntennaConstructorScreen(name="antedit"))
+        sm.add_widget(SatelliteConstructorScreen(name="satedit"))
         return sm
 
     def _update_bg_rect(self, instance, value):
--- main.py
+++
@@ -1,6 +0,0 @@
-from pyproj import Transformer, Geod #–азобратьс€ с библиотекой, возможно реализовать несколько систем координат?
-from typing import  Type
-
-from Classes import Transmitter 
-
-
