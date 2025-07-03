f059d1cfde75b067690cb662dba24a2c255fbf98
Asi-glitch <grishinalex2006@mail.ru>
2025-07-03 23:22:49 +0700

    Le le fin
    Точно гиперласт(мне уже плохо)

--- Classes/Transmitter.py
+++ Classes/Transmitter.py
@@ -38,10 +38,6 @@ class Transmitter(object):
         # Вычисляет площадь поверхности параболической антенны
         D = self.antennaDiameter
         f = self.focusOfAntenna
-
-        term = (1 + (4 * f**2) / D**2)**1.5
-        return (math.pi * D**3 / (48 * f**2)) * (term - 1)
-
         try:
             term1 = (f + D**2/(16*f))**1.5
             term2 = f**1.5
@@ -62,12 +58,6 @@ class Transmitter(object):
     
     def get_ecef_coordinates(self):
         if self.IsGround:
-
-            result = self._geoid_transformer.transform(self.pos1, self.pos2, self.pos3)
-            h_ellipsoid = result[2] #Высота над эллипсоидом из кортежа 
-            return self._transformer.transform(self.pos1, self.pos2, h_ellipsoid)
-
-
             return self._geoid_transformer.transform(xx = self.pos2, yy = self.pos1,  zz = self.pos3)
         else:
             return (self.pos1, self.pos2, self.pos3)
@@ -78,32 +68,6 @@ class Transmitter(object):
         other_coords = other.get_ecef_coordinates()
         return math.sqrt(sum((a-b)**2 for a, b in zip(self_coords, other_coords))) #zip для красоты, тут он попарно вычисляет квадрат разности координат
 
-
-    def Calculate_signal_noise_ratio(self, other_ant, l2, l3, l4, l5, rate_of_transm, temperature): #self- земной!!!! если нужно обработать еще случай, когда он космический то сообщи.
-        Knd_earth = self.convert_to_decibel(self.coefficientOfDirectedAction)
-        Knd_space = self.convert_to_decibel(other_ant.coefficientOfDirectedAction)
-        Sat_distance = self.Calculate_distance_to(self, other_ant)
-        l1= self.convert_to_decibel(16*(3.1415**2)*Sat_distance**2/(self.waveLength**2))
-        EIRP_self = self.outputOfTransmitter + Knd_earth - self.lossInTransmitter
-        Accepted_isotropic_power = EIRP_self - l1 - l2 - l3
-        Power_of_recived_signal = Accepted_isotropic_power + Knd_space - l4
-        temperature_decibel = self.convert_to_decibel(temperature)
-        Spectral_velocity_of_noice = -228.6 + temperature_decibel
-        Signal_noise_ratio = Power_of_recived_signal - Spectral_velocity_of_noice - rate_of_transm - l5
-        return Signal_noise_ratio
-
-    def Calculate_signal_noise_ratio(self, other_ant, l2, l3, l4, l5, rate_of_transm, temperature): 
-        Knd_earth = self.to_db(self.coefficientOfDirectedAction)
-        Knd_space = self.to_db(other_ant.coefficientOfDirectedAction)
-        Sat_distance = self.Calculate_distance_to(other_ant)
-        l1= self.to_db(16*(math.pi**2)*Sat_distance**2 / (self.waveLength**2))
-        EIRP_self = self.outputOfTransmitter + Knd_earth - self.lossInTransmitter
-        Accepted_isotropic_power = EIRP_self - l1 - float(l2) - float(l3)
-        Power_of_recived_signal = Accepted_isotropic_power + float(Knd_space) - float(l4)
-        temperature_decibel = self.to_db(float(temperature))
-        Spectral_velocity_of_noice = -228.6 + temperature_decibel
-        Signal_noise_ratio = Power_of_recived_signal - Spectral_velocity_of_noice - self.to_db(float(rate_of_transm)) - float(l5)
-        return round(Signal_noise_ratio, 2)
 
 
     def Calculate_signal_noise_ratio(self, other_ant, l2, l3, l4, l5, rate_of_transm, temperature): 
